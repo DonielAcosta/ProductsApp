038e106f2209eb8a2d13b995ad52ced27c3f1c48
Doniel Acosta <donielacosta1995@gmail.com>
2025-05-28 15:42:10 -0400

    interfaces y peticiones http

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -2,10 +2,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Button, Icon, Layout, Text } from '@ui-kitten/components';
 import { useAuthStore } from '../../store/auth/useAuthStore';
+import { getProductsByPage } from '../../../actions/auth/products/get-products-by-page';
 
 export const HomeScreen = () => {
 
   const {logout} = useAuthStore();
+
+  getProductsByPage(0);
   return (
     <Layout style={{ flex:1, justifyContent:'center',alignItems:'center' }}>
         <Text>HomeScreen</Text>
